9420f9d16f4b804b040340baf5c69a67542bce8d
Krzysztof Banasiak <Krzysztof Banasiak>
2017-03-26 18:01:05 +0200

    Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import ocr.Ocr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ocr ocr = new Ocr();
+        ocr.start();
+    }
+}
---
+++ src/ocr/Ocr.java
@@ -0,0 +1,10 @@
+package ocr;
+
+/**
+ * Created by krzys on 3/26/2017.
+ */
+public class Ocr {
+    public void start() {
+
+    }
+}
